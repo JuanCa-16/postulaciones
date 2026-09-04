68f0ed37c3925712f8d30274a9f8dfdae3c4646a
JuanCa-16 <juancamilohg162003@gmail.com>
2026-09-04 11:54:21 -0500

    feat: update CORS configuration to allow requests from production URL

--- src/main/java/com/postulaciones/postulaciones/config/SecurityConfig.java
+++ src/main/java/com/postulaciones/postulaciones/config/SecurityConfig.java
@@ -60,8 +60,9 @@ public class SecurityConfig {
 
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(
-                                List.of("http://localhost:4200"));
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:4200",
+                                "https://postuya.vercel.app"));
 
                 configuration.setAllowedMethods(
                                 List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
